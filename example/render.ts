54ba129feeb1c422019b5ff5370e602818dc987f
Jan <jan@ninoko.ch>
2022-03-22 10:32:55 +0100

    fix render example

--- example/render.ts
+++ example/render.ts
@@ -1,21 +1,22 @@
 import { Blender } from '../src/index'
-import { join, dirname } from 'path'
+import { join } from 'path'
 
-const input  = '../../example/model/model.blend'
-const output = join(dirname(__filename), '../../../output.png')
+const input  = join(__dirname, '..', '..', '..', 'example', 'model', 'model.blend').replace(/\\/g,"/")
+const output = join(__dirname, '..', '..', '..', 'example', 'output', 'output.png').replace(/\\/g,"/")
+
 console.log('starting blender')
 const bpy = new Blender()
-console.log('opening file')
+console.log(`opening file ${input}`)
 bpy.ops.wm.open_mainfile({ filepath: input })
 bpy.context.scene.render.filepath = output
 bpy.context.scene.render.resolution_x = 640
 bpy.context.scene.render.resolution_y = 480
 bpy.context.scene.cycles.device = 'CPU'
 bpy.context.scene.cycles.samples = 32
-console.log('rendering...')
+console.log(`rendering to ${output}...`)
 bpy.ops.render.render({ 
     write_still: true, 
-    use_viewport: true 
+    use_viewport: true
 })
 
 bpy.dispose()
